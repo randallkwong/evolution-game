aee06d579256cea3e25fac2ae839d3106019fcc8
Randy Wong <61926295+randallkwong@users.noreply.github.com>
2020-03-24 19:06:06 -0700

    Creates classes for Cards and Species

---
+++ Card.java
@@ -0,0 +1,16 @@
+
+public class Card {
+
+	// The amount of food this trait card is worth when played into the watering hole.
+	int foodValue;
+	
+	public Card(int foodValueInput) {
+		
+		this.foodValue = foodValueInput;
+				
+	}
+
+
+	
+	
+}
---
+++ Species.java
@@ -0,0 +1,29 @@
+
+public class Species {
+
+	// The population of a species.
+	int population;
+	
+	// The body size of a species.
+	int bodySize;
+	
+	// The maximum amount of food the species can consume.
+	int foodCapacity;
+	
+	// THe current amount of food the species is storing.
+	int currentFoodConsumed;
+	
+	// Traits
+	boolean isCarnivore;
+	boolean hasFatTissue;
+	boolean hasClimbing;
+
+	public Species() {
+		
+		this.population = 1;
+		this.bodySize = 1;
+		this.isCarnivore = false;
+		
+	}
+	
+}
