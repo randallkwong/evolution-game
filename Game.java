6408cdeeb61132e1fb3ff373381da7bce2af7a2f
Randy Wong <61926295+randallkwong@users.noreply.github.com>
2020-03-24 19:30:04 -0700

    Adds Game class and WateringHole class

---
+++ Game.java
@@ -0,0 +1,15 @@
+
+public class Game {
+	
+	WateringHole wateringHole = new WateringHole();
+	
+	// Create and shuffle deck
+	
+	// Deal 4 cards
+	
+	// Create starting game species
+
+	// Turn actions loop
+	
+	
+}
---
+++ WateringHole.java
@@ -0,0 +1,19 @@
+
+public class WateringHole {
+
+	// The current number of food tokens in the watering hole.
+	int currentFoodAvailable;
+	
+	public WateringHole() {
+		this.currentFoodAvailable = 0;
+	}
+
+	public int getCurrentFoodAvailable() {
+		return currentFoodAvailable;
+	}
+
+	public void setCurrentFoodAvailable(int currentFoodAvailable) {
+		this.currentFoodAvailable = currentFoodAvailable;
+	}
+	
+}
