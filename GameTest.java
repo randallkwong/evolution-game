189d28be0ac743cb551272615d31f530ab4e609c
Randy Wong <61926295+randallkwong@users.noreply.github.com>
2020-04-05 15:22:19 -0700

    Adds Game test to check that species starve and die off correctly and that the board updates as it is supposed to.

--- GameTest.java
+++ GameTest.java
@@ -15,11 +15,29 @@ class GameTest {
 		// Create player
 		Player playerOne = new Player(1);
 		
+		// Board creation creates one species
 		SpeciesBoard SpeciesBoard1 = new SpeciesBoard();
-		SpeciesBoard1.addNewSpeciestoLeft();
 		
+		// Add one additional species to the board
+		SpeciesBoard1.addNewSpeciestoLeft();
+
+		// Two species are in play
+		assertEquals(SpeciesBoard1.numberOfSpeciesInPlay(), 2);
+		
+		// Feed one of the species
+		SpeciesBoard1.updateFoodConsumed(1);
+		
+		// Handle starvation
 		currentGame.starveSpecies(playerOne, SpeciesBoard1);
 		
+		// One species should starve and die off, leaving one species left
+		assertEquals(SpeciesBoard1.numberOfSpeciesInPlay(), 1);
+
+		// Move food consumed off of the species that was fed
+		SpeciesBoard1.moveFoodConsumed(1, 1);
+		
+		// Feed no species this time and handle starvation again. No species should be remaining
+		currentGame.starveSpecies(playerOne, SpeciesBoard1);
 		assertEquals(SpeciesBoard1.numberOfSpeciesInPlay(), 0);
 		
 	}
