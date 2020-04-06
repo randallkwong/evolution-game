7f6f85fd36ab33475877cf8446074d77f2c64b3e
Randy Wong <61926295+randallkwong@users.noreply.github.com>
2020-04-05 17:44:45 -0700

    Adds java docs for the Hand class.

--- Hand.java
+++ Hand.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/**
+ * 
+ * The Hand class keep track of each player's hand, which contains their trait cards.
+ *
+ */
 public class Hand {
 	ArrayList<Card> hand = new ArrayList<Card>();
 	
@@ -7,6 +12,16 @@ public class Hand {
 		
 	}
 
+	/**
+	 * This method allows a player to draw cards from the deck.
+	 * 
+	 * @param numberOfCardsToDraw
+	 * Takes the number of cards to draw from the deck.
+	 * 
+	 * @param deck
+	 * Takes the deck to update as cards are removed and transferred to a player's hand.
+	 * 
+	 */
 	public void drawCards(int numberOfCardsToDraw, Deck deck) {	
 
 		ArrayList<Card> tempHand = getHand();	
@@ -21,14 +36,29 @@ public class Hand {
 
 	}
 	
+	
+	/**
+	 * 
+	 * @return
+	 * Return's the hand
+	 */
 	public ArrayList<Card> getHand() {	
 		return this.hand;
 	}
-	
+
+	/**
+	 * Updates the active hand.
+	 * 
+	 * @param hand
+	 * Takes a hand to replace the active hand with.
+	 */
 	public void setHand(ArrayList<Card> hand) {	
 		this.hand = hand;	
 	}
 	
+	/**
+	 * This method prints out the Cards in a player's Hand.
+	 */
 	public void displayHand() {
 		System.out.println();
 		for (int i = 0; i < hand.size(); i++) {
@@ -37,6 +67,12 @@ public class Hand {
 		System.out.println("");
 	}
 	
+	/**
+	 * This method removes a card from a player's hand.
+	 * 
+	 * @param CardNum
+	 * Takes the index of the card to remove based on a player's input.
+	 */
 	public void removeCardfromHand(int CardNum) {
 		hand.remove(CardNum -1);
 		//add the logic to check for empty hand.
@@ -47,10 +83,25 @@ public class Hand {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * Returns the number of Cards in the Hand.
+	 * 
+	 */
 	public int getHandSize() {
 		return hand.size();
 	}
 	
+	/**
+	 * 
+	 * @param CardNum
+	 * Takes the index of the Card based on the player's input.
+	 * 
+	 * @return
+	 * Returns the food value of the Card.
+	 * 
+	 */
 	public int getValuefromCard(int CardNum) {
 		return hand.get(CardNum-1).getCardValue();
 	}
