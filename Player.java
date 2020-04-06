2c55610cb3b75b90eb2ab23f512753228246b10f
Randy Wong <61926295+randallkwong@users.noreply.github.com>
2020-04-05 17:49:58 -0700

    Adds java docs for the Player class.

--- Player.java
+++ Player.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 
+/**
+ * 
+ * This class defines a Player of the Evolution game.
+ * It keeps track of things like the player's score and the player's status for each play phase.
+ *
+ */
 public class Player {
 
 	// Ex: Player 1, Player 2
@@ -25,6 +31,12 @@ public class Player {
 	
 	//ArrayList<Species> activeSpecies = new ArrayList<Species>();
 	
+	/**
+	 * This constructs a player and sets all of the starting values for the game for that Player.
+	 * 
+	 * @param playerNumberInput
+	 * Takes a player number and assigns it to the constructed Player.
+	 */
 	public Player(int playerNumberInput) {
 		
 		this.playerNumber = playerNumberInput;
@@ -34,6 +46,9 @@ public class Player {
 		this.endPhaseThree = 0;
 	}
 
+	/**
+	 * This method prints out a message to let the player know it is their turn.
+	 */
 	public void phaseThreeStart() {
 		System.out.println("Player " + playerNumber + ", it's your turn!");
 		System.out.println("Would you like to: ");
@@ -41,42 +56,84 @@ public class Player {
 		System.out.println("2) Play a card");
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * Returns whether or not a player is finished with play Phase 3.
+	 */
 	public int getPhaseThreeStatus() {
 		return this.endPhaseThree;
 	}
 
+	/**
+	 * Sets a player's status as being finished with play Phase 3.
+	 */
 	public void endPhaseThree() {
 		this.endPhaseThree = 1;
 	}
 
+	/**
+	 * Sets a player's status as ready to start play Phase 3.
+	 */
 	public void readyForPhaseThree() {
 		this.endPhaseThree = 0;
 	}
-	
+
+	/**
+	 * 
+	 * @return
+	 * Returns a player's number.
+	 */
 	public int getPlayerNumber() {
 		return this.playerNumber;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * Returns whether a player is currently still feeding species.
+	 */
 	public int getIsFeeding() {
 		return this.isFeeding;
 	}
 
+	/**
+	 * Sets a player as ready to feed their species.
+	 */
 	public void isReadyToFeed() {
 		this.isFeeding = 1;
 	}	
-	
+
+	/**
+	 * Sets a player's status as finished feeding their species.
+	 */
 	public void isDoneFeeding() {
 		this.isFeeding = 0;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * Returns a player's current number of food points.
+	 */
 	public int getFoodPoints() {
 		return foodPoints;
 	}
 
+	/**
+	 * 
+	 * @param foodPoints
+	 * Takes the number of food points and updates the player's current food pionts.
+	 */
 	public void setFoodPoints(int foodPoints) {
 		this.foodPoints = foodPoints;
 	}
 
+	/**
+	 * 
+	 * @param foodPointsToAdd
+	 * Takes the number of food points provided and adds it to a player's total number of food points stored in their food bag.
+	 */
 	public void addFoodPoints(int foodPointsToAdd) {
 		this.foodPoints = this.foodPoints + foodPointsToAdd;
 	}
