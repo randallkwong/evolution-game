9074c0fe9edf0f280e3ea118e02c983b6c99c181
Randy Wong <61926295+randallkwong@users.noreply.github.com>
2020-04-05 18:06:48 -0700

    Adds java docs for the SpeciesBoard class.

--- SpeciesBoard.java
+++ SpeciesBoard.java
@@ -3,23 +3,36 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * 
+ * This class keeps track of the species in play (on the SpeciesBoard).
+ * Each player has a species board containing their living Species.
+ *
+ */
 public class SpeciesBoard {
 	HashMap <Integer, Species> newPlayerBoard;
 	Species newSpecies = new Species(1,1,1,0,false,false,false,0, new ArrayList<String>());
-	
+
+	/**
+	 * When the SpeciesBoard is first constructed at the game's start, each player begins with one Species.
+	 */
 	public SpeciesBoard(){
 		newPlayerBoard = new HashMap <Integer, Species>();
 		newPlayerBoard.put(1,newSpecies);
 	}
 	
-	//add a new species to the player board on the right hand side
+	/**
+	 * This method adds a new species to the player board on the right hand side.
+	 */
 	public void addNewSpeciestoRight() {
 		int currentNumofSpecies = newPlayerBoard.size();
 		newPlayerBoard.put(currentNumofSpecies+1, newSpecies);
 	}
 	
 	
-	//add a new species to the player board on the left hand side
+	/**
+	 * This method adds a new species to the player board on the left hand side.
+	 */
 	public void addNewSpeciestoLeft() {
 		for (int i = newPlayerBoard.size(); i > 0; i--) {
 			Species tmp = newPlayerBoard.get(i);
@@ -28,12 +41,20 @@ public class SpeciesBoard {
 		newPlayerBoard.put(1,newSpecies);
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * Returns the number of living species on a player's SpeciesBoard.
+	 */
 	public int numberOfSpeciesInPlay() {
 				
 		return newPlayerBoard.size();
 		
 	}
 	
+	/**
+	 * Prints the active Species and their characteristics on the current SpeciesBoard.
+	 */
 	public void displaySpeciesBoard() {
 		System.out.println(newPlayerBoard.size() + " species on the board." );
 		
@@ -50,7 +71,12 @@ public class SpeciesBoard {
 	}
 	
 	
-	// remove species that has goes to extinction due to population size <= 0
+	/**
+	 * This method removes Species that have gone extinct. This occurs when a Species population goes to zero.
+	 * Those Species are removed from that player's SpeciesBoard.
+	 * 
+	 * TODO: Re-index the living species for a SpeciesBoard.
+	 */
 	public void extinctSpeciesBoard() {
 		Iterator<Integer> iterator = newPlayerBoard.keySet().iterator();
 		while (iterator.hasNext()) {
@@ -62,8 +88,11 @@ public class SpeciesBoard {
 	}
 	
 	
-	/*
-	 * input position of the species
+	/**
+	 * This method updates the body size of a species.
+	 * 
+	 * @param pos
+	 * Takes the index of a Species on the SpeciesBoard from the player input.
 	 */
 	public void updateBodySize(int pos) {
 		Species value = newPlayerBoard.get(pos);
@@ -80,6 +109,15 @@ public class SpeciesBoard {
 		newPlayerBoard.put(pos,newSpecies);
 	}
 	
+	/**
+	 * This method updates the population size of a species.
+	 * 
+	 * @param pos
+	 * Takes the index of a Species on the SpeciesBoard from the player input.
+	 * 
+	 * @param amountChanged
+	 * Takes the amount to alter a species population by.
+	 */
 	public void updatePopulation(int pos, int amountChanged) {
 		Species value = newPlayerBoard.get(pos);
 		int isAlive = value.getIsAlive();
@@ -98,6 +136,12 @@ public class SpeciesBoard {
 		newPlayerBoard.put(pos,newSpecies);
 	}
 	
+	/**
+	 * This method updates the food consumed for a species.
+	 * 
+	 * @param pos
+	 * Takes the index of a Species on the SpeciesBoard from the player input.
+	 */
 	public void updateFoodConsumed(int pos) {
 		Species value = newPlayerBoard.get(pos);
 		int isAlive = value.getIsAlive();
@@ -113,6 +157,15 @@ public class SpeciesBoard {
 		newPlayerBoard.put(pos,newSpecies);
 	}
 
+	/**
+	 * This method updates the food consumed of a species when food is moved from the SpeciesBoard to a player's food bag.
+	 * 
+	 * @param pos
+	 * Takes the index of a Species on the SpeciesBoard from the player input.
+	 * 
+	 * @param foodMoved
+	 * Takes the amount of food moved.
+	 */
 	public void moveFoodConsumed(int pos, int foodMoved) {
 		Species value = newPlayerBoard.get(pos);
 		int isAlive = value.getIsAlive();
@@ -128,6 +181,19 @@ public class SpeciesBoard {
 		newPlayerBoard.put(pos,newSpecies);
 	}
 	
+	/**
+	 * This method updates the traits attached to a species.
+	 * 
+	 * @param pos
+	 * Takes the index of a Species on the SpeciesBoard from the player input.
+	 * 
+	 * @param cardName
+	 * Takes the trait to update.
+	 * 
+	 * @param scan
+	 * Takes the active scanner to capture user input.
+	 * 
+	 */
 	public void updateTraitCard(int pos, String cardName, Scanner scan) {
 		Species value = newPlayerBoard.get(pos);
 		int isAlive = value.getIsAlive();
