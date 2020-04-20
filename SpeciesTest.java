001fd93288a78a0eb4cbcc950172ee654fae5f13
ritasheny <ritashen@10.0.0.111>
2020-04-19 23:58:13 -0700

    added new tests to SpeciesTest.java file

--- SpeciesTest.java
+++ SpeciesTest.java
@@ -26,5 +26,32 @@ class SpeciesTest {
 		assertEquals(testSpeciesDoesNotHaveFatTissue.getFoodCapacity(), 1);
 		
 	}
+	
+	
+	@Test
+	/*
+	 * Tests the ability to add traits and return number of trait cards.
+	 */
+	void testgetTraitsCard() {
+		ArrayList<String>testTraits = new ArrayList<String>();
+		
+		testTraits.add("Carnivore");
+				
+		Species testSpecies = new Species(1, 1, 1, 0, false, false, false, 1, testTraits,"1");
+		assertEquals(testSpecies.getTraitcard(),1);	
+	}
+	
+	@Test
+	/*
+	 * Tests the ability to add Carnivore traits and return the boolean for isCarnivore
+	 */
+	void testaddTraits() {
+		ArrayList<String>testTraits = new ArrayList<String>();
+		testTraits.add("Carnivore");
+		
+		Species testSpecies = new Species(1, 1, 1, 0, true, false, false, 1, testTraits,"1");
+		assertEquals(testSpecies.isCarnivore,true);		
+	}
+	
 
 }
